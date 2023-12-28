8c0646c010d8b18e1a02c279a38ed2d565bb1905
JeonB <newjeonb@gmail.com>
2023-12-28 18:06:45 +0900

    Add email list

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import {
   ShowGuesser,
 } from 'react-admin';
 import { dataProvider } from './dataProvider';
-import { UserList } from './users';
+import { UserEdit, UserList } from './users';
 
 //name
 export const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} edit={EditGuesser} />
+    <Resource name="users" list={UserList} edit={UserEdit} />
   </Admin>
 );
--- src/users.tsx
+++ src/users.tsx
@@ -1,58 +1,136 @@
-import { Stack, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Stack,
+  Typography,
+} from '@mui/material';
 import { useState } from 'react';
 import {
   Datagrid,
+  Edit,
+  InputProps,
+  List,
+  ListContext,
   ListContextProvider,
   Pagination,
-  RecordContextProvider,
+  SimpleForm,
+  TextInput,
   TextField,
+  useInput,
   useListController,
-  useRecordContext,
-  useResourceContext,
-  useShowController,
 } from 'react-admin';
+import SimpleDialogDemo from './userform';
 
-export const UserList = () => {
-  const resource = useResourceContext();
-  const listContext = useListController();
-  const showContext = useShowController({ id: 1 });
-  const [userId, setUserId] = useState(2);
-  const getId = (id, resource, record) => {
-    setUserId(id);
-  };
-  const RecordView = () => {
-    const record = useRecordContext();
+export const UserEdit = () => {
+  const CustomField = (props: InputProps) => {
+    const handleChange = onChange => {
+      return onChange('1111');
+    };
+    const {
+      field,
+      fieldState: { isTouched, invalid, error },
+      formState: { isSubmitted },
+      isRequired,
+    } = useInput({ source: props.source, onChange: handleChange });
     return (
-      <Stack>
-        <Typography>{record.name}</Typography>
-        <Typography>{record.username}</Typography>
-      </Stack>
+      <TextField id="name" label="Outlined" variant="outlined" {...field} />
     );
   };
   return (
-    <Stack>
-      <ListContextProvider value={listContext}>
-        <Datagrid rowClick={getId}>
-          <TextField source="id" />
-          <TextField source="name" />
-          <TextField source="username" />
-          {/* <EmailField source="email" />
-            <TextField source="address.street" />
-            <TextField source="phone" />
-            <TextField source="website" />
-            <TextField source="company.name" /> */}
-        </Datagrid>
+    <Edit>
+      <SimpleForm>
+        <TextInput source="id" />
+        <TextField id="name" label="Outlined" variant="outlined" />
+        <TextInput source="username" />
+        <TextInput source="email" />
+        <TextInput source="address.street" />
+        <TextInput source="phone" />
+        <TextInput source="website" />
+      </SimpleForm>
+    </Edit>
+  );
+};
+export const UserList = () => {
+  const listContext = useListController();
+  const [selectedUser, setSelectedUser] = useState(null);
 
-        <Pagination />
-      </ListContextProvider>
-      <Typography variant="h3">User Detail</Typography>
-      <RecordContextProvider
-        value={{
-          name: 5,
-          username: 'bababapldkfjals',
-        }}>
-        <RecordView />
-      </RecordContextProvider>
+  const handleViewDetails = user => {
+    setSelectedUser(user);
+  };
+
+  const handleCloseDetails = () => {
+    setSelectedUser(null);
+  };
+
+  const UserListItem = ({ data }) => (
+    <Grid item key={data.id} xs={12} sm={6} md={4} mt={3} mr={5}>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" sx={{ marginBottom: 1 }}>
+            {data.username}
+          </Typography>
+          <Typography>{data.name}</Typography>
+          <Typography>{data.email}</Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={() => handleViewDetails(data)}>
+            View
+          </Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
+
+  const UserDetail = () => (
+    <Grid>
+      <Typography variant="h3" sx={{ marginTop: -30 }}>
+        UserDetail
+      </Typography>
+      {selectedUser && (
+        <Card sx={{ marginTop: 2 }}>
+          <CardContent>
+            <Typography variant="h5" sx={{ marginBottom: 1 }}>
+              {selectedUser.username}
+            </Typography>
+            <Typography>{selectedUser.name}</Typography>
+            <Typography>{selectedUser.email}</Typography>
+            <Typography>{selectedUser.address.street}</Typography>
+            <Typography>{selectedUser.phone}</Typography>
+            <Typography>{selectedUser.website}</Typography>
+            <Typography>{selectedUser.company.name}</Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" onClick={handleCloseDetails}>
+              Close
+            </Button>
+          </CardActions>
+        </Card>
+      )}
+    </Grid>
+  );
+
+  return (
+    <Stack>
+      <SimpleDialogDemo />
+      {/* <ListContextProvider value={listContext}>
+        <Typography variant="h3" sx={{ marginTop: 3 }}>
+          UserList
+        </Typography>
+        <Grid container spacing={3} marginBottom={3}>
+          {listContext.data &&
+            listContext.data.map(data => (
+              <UserListItem key={data.id} data={data} />
+            ))}
+          <UserDetail />
+          <Pagination
+            rowsPerPageOptions={[1, 4, 10]}
+            sx={{ marginLeft: 30, marginTop: 3 }}
+          />
+        </Grid>
+      </ListContextProvider> */}
     </Stack>
   );
 };
